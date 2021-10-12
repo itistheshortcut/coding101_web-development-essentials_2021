172c23dd6427ca65f667441394d573fbb03c406f
MostafaIn <mostafa.hazareh@theshortcut.org>
2021-10-12 17:42:20 +0300

    practice files

---
+++ script.js
@@ -0,0 +1,38 @@
+// Variables
+let name = 'mostafa';
+name = 'John';
+let price = 120;
+price = 150;
+let totalPrice = 200;
+totalPrice = 220;
+
+let message = 'hello world!';
+message = 'hello mostafa ssssss!';
+console.log(message);
+
+
+
+// Data Types
+let firstName = 'Mostafa';
+let lastName = 'Hazareh';
+console.log(firstName);
+console.log(typeof lastName);
+
+let milkPrice = 2;
+let applePrice = 10;
+let total = milkPrice + applePrice;
+console.log(typeof milkPrice);
+console.log(typeof applePrice);
+console.log(total);
+
+let isLoading = false;
+isLoading = true;
+console.log(typeof isLoading);
+
+let address = null;
+address = 'helsinki';
+console.log(typeof address);
+
+let city;
+city = 12
+console.log(typeof city);
