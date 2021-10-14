d08abfe195a31b6834bb7d5c5804f5a5e815b085
MostafaIn <mostafa.hazareh@theshortcut.org>
2021-10-14 16:33:15 +0300

    last slide

--- script.js
+++ script.js
@@ -36,3 +36,44 @@ console.log(typeof address);
 let city;
 city = 12
 console.log(typeof city);
+
+
+// Array
+const names = ["Mostafa", "John", "Ahmed", "Bill", 123,33, null, true];
+names.sort()
+console.log(names)
+
+const ages = [32, 21, 11, 10, 201];
+console.log(ages);
+
+// Object
+const myAddress = {
+    country: 'Finland',
+    city: 'Helsinki',
+    postalCode: 001
+};
+console.log(myAddress);
+
+const student = {
+    id: 01001,
+    name: 'Mostafa',
+    email: 'mostafa@theshortcut.org',
+    phone: 21435438
+}
+console.log(student);
+
+// functions
+let num1 = 120;
+let num2 = 15;
+let num3 = 112;
+const addNumbers = function () {
+    console.log(num1 + num2 + num3);
+}
+addNumbers();
+
+const getAvg = () => {
+    let avg = (num1 + num2 + num3) / 3;
+    console.log(avg)
+};
+getAvg();
+
