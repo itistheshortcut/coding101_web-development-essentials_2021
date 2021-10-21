58d584a494940cf0dc5a83d19776413419fbc525
MostafaIn <mostafa.hazareh@theshortcut.org>
2021-10-21 17:37:33 +0300

    practice file

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // JavaScript Function
+
+const recArea = (width, height) => {
+    return width + height;
+};
+
+console.log(recArea(120, 100));
+console.log(recArea(33, 22));
+console.log(recArea(13, 200));
+
+const sendMsg = (message, reciever) => {
+    return `${message} ${reciever}`;
+};
+
+console.log(sendMsg('Hello', 'mostafa'));
+console.log(sendMsg('please help me', 'mona'));
+
+const getAvg = (num1, num2, num3) => {
+    let avg = (num1 + num2 + num3) / 3;
+    return avg;
+}
+console.log(getAvg(12, 10, 30));
+console.log(getAvg(2, 4, 6));
+console.log(getAvg(44, 3, 1));
+
+
+
+
+
+
+
+
